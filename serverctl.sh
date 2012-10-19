c8ef566195ac27892a47093492ee0c6b3093199a
Dave Ceddia <dceddia@gmail.com>
2012-10-18 22:03:07 -0400

    added 'status' command to serverctl

--- serverctl.sh
+++ serverctl.sh
@@ -12,12 +12,14 @@ if [[ -z "$1" || -z "$2" ]]; then
   usage
 fi
 
-if [[ "$2" != "start" && "$2" != "stop" ]]; then
+if [[ "$2" != "start" && "$2" != "stop" && "$2" != "status" ]]; then
   usage
 fi
 
 if [[ "$COMMAND" == "start" ]]; then
   vagrant ssh $SERVER -c "sudo apache2ctl start"
-else
+elif [[ "$COMMAND" == "stop" ]]; then
   vagrant ssh $SERVER -c "sudo apache2ctl stop"
+else
+  vagrant ssh $SERVER -c "sudo apache2ctl status"
 fi
